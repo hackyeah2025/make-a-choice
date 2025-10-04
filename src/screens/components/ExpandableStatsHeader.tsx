187044427951c45dda2271be3403c1c297cda49d
Grzegorz Prywatny <grzegorzprywatny@Grzegorzs-MacBook-Pro.local>
2025-10-04 18:54:50 +0200

    style expandable stats header

--- src/screens/components/ExpandableStatsHeader.tsx
+++ src/screens/components/ExpandableStatsHeader.tsx
@@ -3,7 +3,19 @@ import { useState } from "react";
 export default function ExpandableStatsHeader() {
     const [isExpanded, setIsExpanded] = useState(false);
 
-    return <div className="expandable-stats-header">
-
-    </div>
+    return <div style={{
+        height: "100vh", overflow: "hidden", width: "100vw", backdropFilter: `blur(${isExpanded ? 5 : 0}px)`, position: "absolute",
+        top: 0,
+        transition: "backdrop-filter 0.3s"
+    }}>
+        <div className="expandable-stats-header" style={{
+            height: isExpanded ? "80vh" : "10vh",
+            transition: "height 0.3s",
+            position: "relative"
+        }}>
+            <div className="expandable-stats-header--toggle" onClick={() => setIsExpanded(!isExpanded)}>
+                {/**@ts-ignore */}
+                {isExpanded ? <ion-icon name="chevron-up-outline"></ion-icon> : <ion-icon name="chevron-down-outline"></ion-icon>}
+            </div>
+        </div></div>
 }
