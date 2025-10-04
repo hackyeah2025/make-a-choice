aa63b1ef59d345e1dc6831784235c25227e4ac38
Grzegorz Prywatny <grzegorzprywatny@Grzegorzs-MacBook-Pro.local>
2025-10-04 19:43:14 +0200

    fix z-index issue

--- src/screens/components/ExpandableStatsHeader.tsx
+++ src/screens/components/ExpandableStatsHeader.tsx
@@ -6,7 +6,8 @@ export default function ExpandableStatsHeader() {
     return <div style={{
         height: "100vh", overflow: "hidden", width: "100vw", backdropFilter: `blur(${isExpanded ? 5 : 0}px)`, position: "absolute",
         top: 0,
-        transition: "backdrop-filter 0.3s"
+        transition: "backdrop-filter 0.3s",
+        zIndex: 1000
     }}>
         <div className="expandable-stats-header" style={{
             height: isExpanded ? "80vh" : "10vh",
