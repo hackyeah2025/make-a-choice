a4c79df6725f8d8ee50b899f7d0bc75f406e6197
xnameTM <markuskotarba@gmail.com>
2025-10-04 22:14:53 +0200

    charlie chaplin

--- src/screens/components/ExpandableStatsHeader.tsx
+++ src/screens/components/ExpandableStatsHeader.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import useStats from "../../hooks/useStats";
 
 export default function ExpandableStatsHeader() {
-    const [isExpanded, setIsExpanded] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(true); // TODO: change to false
+    const stats = useStats();
 
     return <div style={{
         height: "100vh", overflow: "hidden", width: "100vw", backdropFilter: `blur(${isExpanded ? 5 : 0}px)`, position: "absolute",
@@ -14,6 +16,45 @@ export default function ExpandableStatsHeader() {
             transition: "height 0.3s",
             position: "relative"
         }}>
+            <div>
+                <div>
+                    <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "128px",
+                }}>
+                    <div style={{
+                        background: "#fff",
+                        borderRadius: "50%",
+                        display: "grid",
+                        placeItems: "center",
+                        aspectRatio: 1,
+                        width: "128px",
+                    }}>
+                        {/**@ts-ignore */}
+                        <ion-icon style={{ fontSize: "128px", color: "#000" }} name="person-circle-outline"></ion-icon>
+                    </div>
+                    <div style={{
+                        background: "#fff",
+                        paddingTop: "64px",
+                        marginTop: "-64px",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "end",
+                        paddingBottom: "8px",
+                        borderBottomLeftRadius: "16px",
+                        borderBottomRightRadius: "16px",
+                    }}>
+                        {/**@ts-ignore */}
+                        <ion-icon style={{ fontSize: "48px", color: "#000" }} name="woman-outline"></ion-icon>
+                        {/**@ts-ignore */}
+                        <ion-icon style={{ fontSize: "32px", color: "#000", marginLeft: -12 }} name="man-outline"></ion-icon>
+                        {/**@ts-ignore */}
+                        <ion-icon style={{ fontSize: "32px", color: "#000", marginLeft: -12 }} name="man-outline"></ion-icon>
+                    </div>
+                </div>
+                </div>
+            </div>
             <div className="expandable-stats-header--toggle" onClick={() => setIsExpanded(!isExpanded)}>
                 {/**@ts-ignore */}
                 {isExpanded ? <ion-icon name="chevron-up-outline"></ion-icon> : <ion-icon name="chevron-down-outline"></ion-icon>}
