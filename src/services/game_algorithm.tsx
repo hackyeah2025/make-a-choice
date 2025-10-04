4fff779094799f40965c87f9e0b8073c0d4be13c
btrojan-official <bartosztrojanofficial@gmail.com>
2025-10-04 20:15:43 +0200

    feat: probably working game algorithm

--- src/services/game_algorithm.tsx
+++ src/services/game_algorithm.tsx
@@ -1,49 +1,46 @@
-// interface Consequence {
-//   impacted: "happiness" | "relations" | "money" | "health";
-//   value: number;
-// }
+export class GameAlgorithm {
+  private events = [
+    "home",
+    "job/school",
+    "night accident",
+    "good luck",
+    "family",
+    "sport",
+    "driving",
+    "trip",
+    "morning routine",
+    "evening",
+  ];
 
-// export class GameAlgorithm {
-//   private events = [
-//     "home",
-//     "job/school",
-//     "night accident",
-//     "good luck",
-//     "family",
-//     "sport",
-//     "driving",
-//     "trip",
-//     "morning routine",
-//     "evening",
-//   ];
+  private impacts = ["happiness", "relations", "money", "health"];
 
-//   private impacts: String[] = ["happiness", "relations", "money", "health"];
+  public generateEvent(): string {
+    const index = Math.floor(Math.random() * this.events.length);
+    return this.events[index];
+  }
 
+  public generateConsequences(count: number = 3): Record<string, any>[] {
+    const consequences: Record<string, any>[] = [];
+    for (let i = 0; i < count; i++) {
+      const impacted = this.impacts[Math.floor(Math.random() * this.impacts.length)];
 
-//   public generateEvent(): string {
-//     const index = Math.floor(Math.random() * this.events.length);
-//     return this.events[index];
-//   }
+      let value = 0;
+      while (value === 0) {
+        value = Math.floor(Math.random() * 21) - 10;
+      }
 
+      consequences.push({
+        impacted: impacted,
+        value: value,
+      });
+    }
+    return consequences;
+  }
 
-//   public generateConsequences(count: number = 3): Consequence[] {
-//     const consequences: Consequence[] = [];
-//     for (let i = 0; i < count; i++) {
-//       const impacted = this.impacts[Math.floor(Math.random() * this.impacts.length)];
-      
-//       let value = 0;
-//       while (value === 0) {
-//         value = Math.floor(Math.random() * 21) - 10;
-//       }
-//       consequences.push({ impacted, value });
-//     }
-//     return consequences;
-//   }
-
-//   public generateScenario() {
-//     return {
-//       event: this.generateEvent(),
-//       consequences: this.generateConsequences(),
-//     };
-//   }
-// }
+  public generateScenario() {
+    return {
+      event: this.generateEvent(),
+      consequences: this.generateConsequences(),
+    };
+  }
+}
