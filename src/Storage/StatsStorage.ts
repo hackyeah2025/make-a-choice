b3a005149ef7386ac85e2038dabe2323dca234cd
btrojan-official <bartosztrojanofficial@gmail.com>
2025-10-05 09:58:44 +0200

    fix: better starting values

--- src/Storage/StatsStorage.ts
+++ src/Storage/StatsStorage.ts
@@ -41,10 +41,10 @@ const STATS_KEY = "stats";
 export const initialStats: Stats = {
     name: "Player",
     age: 15,
-    health: 1,
-    relations: 1,
-    happiness: 1,
-    money: 1,
+    health: 50,
+    relations: 50,
+    happiness: 50,
+    money: 100,
     priorities: [],
 
     income: 0,
